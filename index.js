4171cf6e976b3b0753444118b923ef293e73368c
tetsukamen <tetsukamen00@gmail.com>
2021-11-09 19:21:48 +0900

    Format mac data

--- index.js
+++ index.js
@@ -1,83 +1,104 @@
 const puppeteer = require("puppeteer");
 
-async function getData(query, array=false) {
+async function mac() {
+  console.log("*** Mac ***");
+  const query = {
+    url: "https://www.apple.com/jp/shop/refurbished/mac/16gb",
+    itemSelector: "li.as-producttile",
+    nameSelector: "a.as-producttile-tilelink",
+    priceSelector: "div.as-producttile-currentprice",
+    pagenation: "span.as-pagination-totalnumbers",
+  };
+
   const browser = await puppeteer.launch({ headless: false }); // browser起動
   const page = await browser.newPage(); // ページ生成
   await page.goto(query.url); // ページへ移動
 
-  let text;
-  if(array){
-    text = await page.$$eval(query.querySelector, (list) => {
-      return list.map(l=>l.textContent);
-    });
-  }else{
-    // テキスト取得
-    text = await page.$eval(query.querySelector, (item) => {
+  // ページ数の取得
+  const pageAmount = parseInt(
+    await page.$eval(query.pagenation, (item) => {
       return item.textContent;
-    });
-  }
-  browser.close();
-  return text;
-}
+    })
+  );
+  console.log("pageAmount", pageAmount);
 
-async function getMac(query){
-  const browser = await puppeteer.launch({ headless: false }); // browser起動
-  const page = await browser.newPage(); // ページ生成
-  await page.goto(query.url); // ページへ移動
+  const originalData = []; // 収集した生データ
 
-  const pageAmount = parseInt(await page.$eval("span.as-pagination-totalnumbers",(item)=>{
-    return item.textContent;
-  }))
-  console.log("pageAmount", pageAmount)
-
-  let data = [];
-  
   // １ページ目のスクレイピング
-  const dataFirstPage = await page.$$eval(query.querySelector,(list)=>{
-    return list.map(l=>l.textContent);
-  })
-  data = data.concat(dataFirstPage);
+  const list = await page.$$(query.itemSelector);
+  for (let i = 0; i < list.length; i++) {
+    // 製品名
+    const a = await list[i].$(query.nameSelector);
+    const name = await (await a.getProperty("textContent")).jsonValue();
+    const link = await (await a.getProperty("href")).jsonValue();
+    // 価格
+    const div = await list[i].$(query.priceSelector);
+    const price = await (await div.getProperty("textContent")).jsonValue();
 
-  for(let i=0; i<pageAmount-1; i++){
+    originalData.push({ name, price, link });
+  }
+
+  for (let i = 0; i < pageAmount - 1; i++) {
     // ページネーションのクリック
     await page.click("button.paddlenav-arrow.paddlenav-arrow-next");
     await page.waitForTimeout(1000);
-    await page.waitForSelector(query.querySelector);
-  
+    await page.waitForSelector(query.itemSelector);
+
     // スクレイピング
-    const dataNextPage = await page.$$eval(query.querySelector,(list)=>{
-      return list.map(l=>l.textContent);
-    })
-    data = data.concat(dataNextPage);
+    const list = await page.$$(query.itemSelector);
+    for (let i = 0; i < list.length; i++) {
+      // 製品名
+      const a = await list[i].$(query.nameSelector);
+      const name = await (await a.getProperty("textContent")).jsonValue();
+      const link = await (await a.getProperty("href")).jsonValue();
+      // 価格
+      const div = await list[i].$(query.priceSelector);
+      const price = await (await div.getProperty("textContent")).jsonValue();
+
+      originalData.push({ name, link, price });
+    }
   }
-  
 
   browser.close();
-  return data;
+
+  // 取得したデータを加工
+  const processedData = originalData.map((item) => {
+    // core数
+    const coreObj = item.name.match(/Core i(\d+)/);
+    const core = coreObj ? parseInt(coreObj[1]) : null;
+    // 価格
+    const priceObj = item.price.match(/(\d|,)+/);
+    const price = priceObj ? parseInt(priceObj[0].replace(",", "")) : null;
+    return { core, price };
+  });
+
+  // 加工したデータを表示
+  console.log(processedData);
 }
 
-async function mac(){
-  console.log("*** Mac ***")
-  const macQuery = {
-    url:"https://www.apple.com/jp/shop/refurbished/mac/16gb",
-    querySelector:"li.as-producttile",
-  }
-  const mac = await getMac(macQuery,true);
-  console.log(mac)
-}
+async function cleaner() {
+  console.log("*** クリーナー ***");
 
-async function cleaner(){
-  console.log("*** クリーナー ***")
-  const cleanerQuery = {
+  const query = {
     url: "https://www.galliumwebshop.com/p/item-detail/detail/i68.html",
-    querySelector: ".itemstock .i_value"
-  }
+    querySelector: ".itemstock .i_value",
+  };
 
-  const cleaner = await getData(cleanerQuery);
-  if(isNaN(cleaner)){
-    console.log("在庫なし")
-  }else{
-    console.log("在庫:",cleaner);
+  const browser = await puppeteer.launch({ headless: false }); // browser起動
+  const page = await browser.newPage(); // ページ生成
+  await page.goto(query.url); // ページへ移動
+
+  // 在庫テキストの取得
+  const text = await page.$eval(query.querySelector, (item) => {
+    return item.textContent;
+  });
+
+  browser.close();
+
+  if (isNaN(text)) {
+    console.log("在庫なし");
+  } else {
+    console.log("在庫:", cleaner);
   }
 }
 
@@ -85,11 +106,10 @@ async function main() {
   try {
     // const url =
     //   "https://jp.hotels.com/search.do?f-accid=1&q-destination=%E6%97%A5%E6%9C%AC%E3%80%81%E6%96%B0%E6%BD%9F%E7%9C%8C%E5%8D%97%E9%AD%9A%E6%B2%BC%E5%B8%82&q-check-in=2021-12-30&q-check-out=2022-01-04&q-rooms=1&q-room-0-adults=1&q-room-0-children=1&q-room-0-child-0-age=11";
-    
-    await mac();
-    
-    // await cleaner();
 
+    await mac();
+
+    // await cleaner();
   } catch (e) {
     console.log(e);
   }
